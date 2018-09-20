8bcea816e7e5e1434efc63d0373eeacb5c826df5
11mink <11mink@naver.com>
2018-09-19 21:51:44 -0700

    really really final

--- main.cpp
+++ main.cpp
@@ -5,29 +5,35 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 
-void dump_payload(u_char*p, int len){
-	if(len<=0) return;
-	printf("[Data] : \n");
-	for(int i=0; i< len; i++){
-		if(i >= 32) break;
-		printf("%02x ", *p);
-		p++;
-		if(i == 15)
-			printf("\n");
+int min(int a, int b){
+	if(a<b) return a;
+	return b;
+}
+
+void dump_data(u_char*p, int len){
+	if(len == 0){ 
+		printf("None\n");
+		return;
 	}
 	printf("\n");
+	for(int i=0; i< len; i++){
+		printf("%02x ", *p);
+		p++;
+		if((i&0x0f) == 0x0f)
+			printf("\n");
+	}
 }
 
 void print_mac(u_char * p){
 		printf("%02x:%02x:%02x:%02x:%02x:%02x\n",p[0],p[1],p[2],p[3],p[4],p[5]);
 }
 	
-void print_packet(u_char * p, int p_size){
+void print_packet(u_char * p){
 	struct ether_header * ehdr = (struct ether_header *) p;
 	struct ip * ihdr = (struct ip*)(p+sizeof(ether_header));
 	struct tcphdr * thdr = (struct tcphdr*)(p+sizeof(ether_header)+ihdr->ip_hl*4);
-	int hdrsize = sizeof(ether_header)+ihdr->ip_hl*4+thdr->th_off*4;
-	u_char * pyld = p + hdrsize;
+	int datalen = ntohs(ihdr->ip_len) - ihdr->ip_hl*4 - thdr->th_off*4;
+	u_char * data = p + sizeof(ether_header) + ihdr->ip_hl*4 + thdr->th_off*4;
 
 	if (ntohs(ehdr->ether_type) != ETHERTYPE_IP) return;
 	if (ihdr->ip_p != IPPROTO_TCP) return;
@@ -39,7 +45,8 @@ void print_packet(u_char * p, int p_size){
 	printf("[IP dst] : %s\n", inet_ntoa(ihdr->ip_dst));
 	printf("[Port src] : %hu\n", ntohs(thdr->th_sport));
 	printf("[Port dst] : %hu\n", ntohs(thdr->th_dport));
-	dump_payload(pyld, p_size - hdrsize);
+	printf("[Data] : ");
+	dump_data(data, min(datalen,32));
 	printf("--------------------------------------------------\n\n\n");
 }
 
@@ -49,14 +56,16 @@ void usage() {
 }
 
 int main(int argc, char * argv[]){
+/*
 	if (argc != 2) {
     usage();
     return -1;
   }
-
+*/
   char* dev = argv[1];
   char errbuf[PCAP_ERRBUF_SIZE];
-  pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+  //pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+  pcap_t * handle = pcap_open_offline("/home/mink/tcp-port-80-test.gilgil.pcap",errbuf);
   if (handle == NULL) {
     fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
     return -1;
@@ -68,7 +77,7 @@ int main(int argc, char * argv[]){
     int res = pcap_next_ex(handle, &header, &packet);
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
-	print_packet((u_char*)packet, header->caplen);
+	print_packet((u_char*)packet);
   }
 
   pcap_close(handle);
